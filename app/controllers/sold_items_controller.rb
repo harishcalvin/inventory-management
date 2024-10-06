7be440014ff4c83ce6056780a3ad12cff9753469
harish <hari991996@gmail.com>
2024-10-06 18:08:57 +0530

    add date filter to sold_items

--- app/controllers/sold_items_controller.rb
+++ app/controllers/sold_items_controller.rb
@@ -1,5 +1,6 @@
 class SoldItemsController < ApplicationController
   def index
     @sales = Sale.includes(sold_items: { product_variant: :product }).order(date: :desc)
+     @sales = @sales.where(date: params[:start_date]..params[:end_date]) if params[:start_date].present? && params[:end_date].present?
   end
 end
