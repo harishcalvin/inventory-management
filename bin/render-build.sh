a0c0db4196bdc5c088a2ec50fc33f2e4422855fb
harish <hari991996@gmail.com>
2024-10-13 15:58:26 +0530

    Add render.yaml

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "Starting build process..."
+
 echo "Installing dependencies..."
 bundle install
 
 echo "Precompiling assets..."
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+RAILS_ENV=production bundle exec rails assets:precompile
+RAILS_ENV=production bundle exec rails assets:clean
 
 echo "Setting up database..."
-bundle exec rails db:create RAILS_ENV=production || true
-bundle exec rails db:migrate:status RAILS_ENV=production
-bundle exec rails db:migrate RAILS_ENV=production
-bundle exec rails db:schema:load RAILS_ENV=production
-bundle exec rails db:seed RAILS_ENV=production
-bundle exec rails db:diagnose RAILS_ENV=production
+RAILS_ENV=production bundle exec rails db:create || true
+RAILS_ENV=production bundle exec rails db:migrate:status
+RAILS_ENV=production bundle exec rails db:migrate
+RAILS_ENV=production bundle exec rails db:schema:load
+RAILS_ENV=production bundle exec rails db:seed
 
 echo "Diagnosing database..."
-echo "Database setup complete."
+RAILS_ENV=production bundle exec rails db:diagnose
+
+echo "Build process completed."
--- config/puma.rb
+++ config/puma.rb
@@ -20,18 +20,20 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
-threads threads_count, threads_count
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
-# Allow puma to be restarted by `bin/rails restart` command.
+port ENV.fetch("PORT") { 3000 }
+
+environment ENV.fetch("RAILS_ENV") { "development" }
+
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
 plugin :tmp_restart
 
-# Specify the PID file. Defaults to tmp/pids/server.pid in development.
-# In other environments, only set the PID file if requested.
-pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
 if ENV["RAILS_ENV"] == "production"
   workers ENV.fetch("WEB_CONCURRENCY") { 4 }
   preload_app!
