df625974f6fb88fc38df51e36ec3bed9d08a7228
harish <hari991996@gmail.com>
2024-10-13 13:01:36 +0530

    update db setup in render build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,4 +4,4 @@
      bundle exec rails assets:precompile
      bundle exec rails assets:clean
      # Uncomment the following line if using a Free instance:
-     bundle exec rails db:migrate
+    bundle exec rails db:create db:schema:load db:seed
