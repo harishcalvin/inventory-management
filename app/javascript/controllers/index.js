5c091b56a384b44bf0b7325a01301eca20dda423
harish <hari991996@gmail.com>
2024-10-14 19:47:39 +0530

    recompile assets for production

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,15 +1,17 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application";
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
+
 eagerLoadControllersFrom("controllers", application);
+
 import NewProductController from "./new_product_controller";
-application.register("new-product", NewProductController);
-// edit
 import EditProductController from "./edit_product_controller";
-application.register("edit-product", EditProductController);
-// sales variant
 import SalesController from "./sales_controller";
-application.register("sales", SalesController);
-//
 import CatalogController from "./catalog_controller";
+
+application.register("new-product", NewProductController);
+application.register("edit-product", EditProductController);
+application.register("sales", SalesController);
 application.register("catalog", CatalogController);
+
+console.log("Registered Stimulus controllers:", application.controllers);
