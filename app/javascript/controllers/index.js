6476fbfa5e86ed805d3ca162334bad781333b341
harish <hari991996@gmail.com>
2024-10-15 10:31:42 +0530

    change import paths

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,12 +2,13 @@
 import { application } from "controllers/application";
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
 
-eagerLoadControllersFrom("controllers", application);
+// Use importmap paths instead of relative paths
+import NewProductController from "controllers/new_product_controller.js";
+import EditProductController from "controllers/edit_product_controller.js";
+import SalesController from "controllers/sales_controller.js";
+import CatalogController from "controllers/catalog_controller.js";
 
-import NewProductController from "./new_product_controller.js";
-import EditProductController from "./edit_product_controller.js";
-import SalesController from "./sales_controller.js";
-import CatalogController from "./catalog_controller.js";
+eagerLoadControllersFrom("controllers", application);
 
 application.register("new-product", NewProductController);
 application.register("edit-product", EditProductController);
