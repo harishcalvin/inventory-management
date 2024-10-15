908fc21cf7e6d2e4365843d2b69387356f8fe827
harish <hari991996@gmail.com>
2024-10-15 10:57:01 +0530

    remove .js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,10 +3,10 @@ import { application } from "controllers/application";
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
 
 // Use importmap paths instead of relative paths
-import NewProductController from "controllers/new_product_controller.js";
-import EditProductController from "controllers/edit_product_controller.js";
-import SalesController from "controllers/sales_controller.js";
-import CatalogController from "controllers/catalog_controller.js";
+import NewProductController from "controllers/new_product_controller";
+import EditProductController from "controllers/edit_product_controller";
+import SalesController from "controllers/sales_controller";
+import CatalogController from "controllers/catalog_controller";
 
 eagerLoadControllersFrom("controllers", application);
 
